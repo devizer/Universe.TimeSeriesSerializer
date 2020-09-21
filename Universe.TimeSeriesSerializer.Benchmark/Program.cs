41831ae4bdba069e1ce97a5847df356753a76e1a
devizer <devizer@users.github.io>
2020-09-21 06:04:33 +0300

    try turn off boundary check again

--- Universe.TimeSeriesSerializer.Benchmark/Program.cs
+++ Universe.TimeSeriesSerializer.Benchmark/Program.cs
@@ -31,8 +31,8 @@ namespace Universe.TimeSeriesSerializer.Benchmark
             }
             else
             {
-                config = config.With(new[] {run.With(Jit.Llvm).With(monoRuntime).WithId("LLVM On")});
-                config = config.With(new[] {run.With(monoRuntime).WithId("LLVM Off")});
+                config = config.With(new[] {run.With(Jit.Llvm).With(monoRuntime).WithId("Mono+LLVM")});
+                config = config.With(new[] {run.With(monoRuntime).WithId("Mono legacy")});
             }
             
             config = config.With(JsonExporter.Custom(fileNameSuffix: "-full", indentJson: true, excludeMeasurements: false));
